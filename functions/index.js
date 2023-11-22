41fd63dc1c9d390d549366f5c0868b6c6f88a2f0
Tremayne Johnson <tremayne4@gmail.com>
2023-11-22 09:59:25 -0500

    stuck

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,19 @@
-import { onRequest } from 'firebase-functions/v2/https'
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-app.get("/test", (res, req) => res.send ("it is working"))
-
-app.get()
-app.post()
-
-export const api = onRequest(app)
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
--- functions/src/connectDb.js
+++ functions/src/connectDb.js
@@ -1,9 +1,8 @@
 
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-
-
 import { creds } from "./creds.js";
+
 
 
 initializeApp ({
