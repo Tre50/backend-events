e465775d215ec17078baf72cea49b674ee3c2bb4
Tremayne Johnson <tremayne4@gmail.com>
2023-11-22 09:11:46 -0500

    final-backend

---
+++ functions/src/connectDb.js
@@ -0,0 +1,14 @@
+
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+
+import { creds } from "./creds.js";
+
+
+initializeApp ({
+    credential: cert(creds),
+});
+
+
+export const db = getFirestore();
